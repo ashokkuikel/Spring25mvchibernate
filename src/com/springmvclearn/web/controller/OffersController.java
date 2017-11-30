7d83d0b5a05d6be9190d27335a6572a4ac36c6a9
dreamspark71 <ashok.kuikel@studentpartner.com>
2017-11-30 11:19:46 +0545

    Enabled production bean to create auto generate

---
+++ src/com/springmvclearn/web/controller/OffersController.java
@@ -0,0 +1,55 @@
+package com.springmvclearn.web.controller;
+
+import java.util.List;
+
+import org.apache.catalina.connector.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.springmvclearn.web.dao.Offer;
+import com.springmvclearn.web.service.OffersService;
+
+@Controller
+public class OffersController {
+
+	private OffersService OffersService;
+
+	// Sending value from controller to page via HttpSession session
+	// @RequestMapping("/")
+	// public String showHome(HttpSession httpSession){
+	// httpSession.setAttribute("name", "Sanjay");
+	// return "home";
+	// }
+
+	// Sending value from controller to page via ModelAndView
+	// @RequestMapping("/")
+	// public ModelAndView showHome() {
+	// ModelAndView modelAndView = new ModelAndView("home");
+	// Map<String, Object> model = modelAndView.getModel();
+	// model.put("name", "I am sanjay");
+	// return modelAndView;
+	// }
+
+	@Autowired
+	public void setOffersService(OffersService offersService) {
+		OffersService = offersService;
+	}
+
+	@RequestMapping("createOffer")
+	public String creatOffer() {
+		return "createOffer";
+	}
+
+	@RequestMapping(value = "offerCreate", method = RequestMethod.POST)
+	public String afterCreate(Model model, Offer offer) {
+		System.out.println(offer);
+		OffersService.create(offer);
+//		List<Offer> offers = OffersService.getCurrent();
+//		model.addAttribute("offers", offers);
+		return "offerCreateMessage";
+	}
+
+}
